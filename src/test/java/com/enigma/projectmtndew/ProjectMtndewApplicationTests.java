5ad2b73393379918d33c03d2b7f0a4e9cdbfffa4
Aditya Rout <adityarout0280@gmail.com>
2026-05-30 16:10:50 +0530

    Initial commit

---
+++ src/test/java/com/enigma/projectmtndew/ProjectMtndewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.projectmtndew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMtndewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
