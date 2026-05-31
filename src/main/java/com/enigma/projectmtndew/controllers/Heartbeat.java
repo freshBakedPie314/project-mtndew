c5ad9f89417a344fb83317a623796e765eda9d37
Aditya Rout <adityarout0280@gmail.com>
2026-05-31 18:17:29 +0530

    ready for deployment

---
+++ src/main/java/com/enigma/projectmtndew/controllers/Heartbeat.java
@@ -0,0 +1,16 @@
+package com.enigma.projectmtndew.controllers;
+
+import lombok.Getter;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("public/heartbeat")
+public class Heartbeat {
+    @GetMapping
+    public ResponseEntity<String> heartbeat() {
+        return ResponseEntity.ok("healthy");
+    }
+}
